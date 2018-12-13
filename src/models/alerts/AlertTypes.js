769649e232b165f5d03704ac27d6044a6b1f3121
刘宗源 <940101379@qq.com>
2018-12-13 20:57:36 +0800

    feat: add register

--- src/models/alerts/AlertTypes.js
+++ src/models/alerts/AlertTypes.js
@@ -4,4 +4,6 @@ export const Prompt = 'prompt';
 export const NamedAccount = 'named_account';
 export const SelectAccount = 'select_authority';
 export const ClaimIdentity = 'claim_identity';
+export const RegisterSuc = 'register_suc';
+
 
