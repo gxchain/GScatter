9043202a6caf4ab635777aef1a502e5213ff0c2b
lanhaoxiang <lanhaoxiang@qq.com>
2018-12-13 20:50:16 +0800

    chore: add explorer link for specified chain_id

--- src/views/IdentitiesView.vue
+++ src/views/IdentitiesView.vue
@@ -29,12 +29,14 @@
 
                 <!-- Account information -->
                 <section class="panel" v-if="Object.keys(identity.accounts).length">
-                    <figure class="header small reverse-margin">accounts</figure>
+                    <figure class="header small reverse-margin">{{locale(langKeys.IDENTITY_AccountHeader)}}</figure>
                     <section class="items" v-for="network in Object.keys(identity.accounts)">
                         <section class="item">
-                            <span>{{networkToName(network)}}</span>
+                            <span v-if="!explorer(network)">{{networkToName(network)}}</span>
+                            <span v-else><a class="link-expo" :href="explorer(network)" target="_blank">{{networkToName(network)}}</a></span>
                             <span>{{identity.accounts[network].formatted()}}</span>
-                            <section class="items token-balances" v-if="loadingTokenBalances && loadingTokenBalances === identity.publicKey">
+                            <section class="items token-balances"
+                                     v-if="loadingTokenBalances && loadingTokenBalances === identity.publicKey">
                                 <section class="item">
                                     Loading token balances
                                 </section>
@@ -53,7 +55,7 @@
 
                 <!-- Personal Information -->
                 <section class="panel" v-if="fullKeysOf(identity.personal).length">
-                    <figure class="header small reverse-margin">personal information</figure>
+                    <figure class="header small reverse-margin">{{locale(langKeys.IDENTITY_PersonalHeader)}}</figure>
                     <section class="items">
                         <section class="item" v-for="key in fullKeysOf(identity.personal)">
                             <span>{{key}}</span>
@@ -65,7 +67,8 @@
                 <!-- Location Information -->
                 <section class="panel" v-for="location in identity.locations">
                     <section v-if="fullKeysOf(location).length">
-                        <figure class="header small reverse-margin">location information</figure>
+                        <figure class="header small reverse-margin">{{locale(langKeys.IDENTITY_LocationHeader)}}
+                        </figure>
                         <section class="items">
                             <section class="item" v-for="key in fullKeysOf(location)">
                                 <span>{{key}}</span>
@@ -81,10 +84,13 @@
                     <section class="actions">
                         <figure v-on:click="goToIdentity(identity)" class="action"><i class="fa fa-pencil"></i></figure>
                         <section v-if="Object.keys(identity.accounts).length">
-                            <figure class="action" @click="showingTokens = identity" v-if="!showingTokensFor(identity)"><i class="fa fa-circle-thin"></i></figure>
-                            <figure class="action" @click="showingTokens = null" v-else><i class="fa fa-times-circle"></i></figure>
+                            <figure class="action" @click="showingTokens = identity" v-if="!showingTokensFor(identity)">
+                                <i class="fa fa-circle-thin"></i></figure>
+                            <figure class="action" @click="showingTokens = null" v-else><i
+                                    class="fa fa-times-circle"></i></figure>
                         </section>
-                        <figure class="action red right" v-on:click="removeIdentity(identity)"><i class="fa fa-minus-square"></i></figure>
+                        <figure class="action red right" v-on:click="removeIdentity(identity)"><i
+                                class="fa fa-minus-square"></i></figure>
                     </section>
                 </section>
 
@@ -94,25 +100,23 @@
 </template>
 
 <script>
-    import Vue from 'vue'
-    import { mapActions, mapGetters, mapState } from 'vuex'
+    import {mapActions, mapGetters, mapState} from 'vuex';
     import * as Actions from '../store/constants';
-    import {RouteNames} from '../vue/Routing'
-    import Identity from '../models/Identity'
-    import Network from '../models/Network'
-    import Scatter from '../models/Scatter'
-    import AlertMsg from '../models/alerts/AlertMsg'
-    import ObjectHelpers from '../util/ObjectHelpers'
-    import AccountService from '../services/AccountService'
-    import PluginRepository from '../plugins/PluginRepository'
+    import {RouteNames} from '../vue/Routing';
+    import Network from '../models/Network';
+    import AlertMsg from '../models/alerts/AlertMsg';
+    import ObjectHelpers from '../util/ObjectHelpers';
+    import PluginRepository from '../plugins/PluginRepository';
 
     export default {
-        data(){ return {
-            searchText:'',
-            balances:[],
-            showingTokens:null,
-            loadingTokenBalances:null,
-        }},
+        data() {
+            return {
+                searchText: '',
+                balances: [],
+                showingTokens: null,
+                loadingTokenBalances: null,
+            };
+        },
         computed: {
             ...mapState([
                 'scatter'
@@ -122,22 +126,29 @@
             ])
         },
         methods: {
-            bind(changed, original) { this[original] = changed },
-            fullKeysOf(obj){ return Object.keys(obj).filter(key => {
-                switch(typeof obj[key]){
-                    case 'string': return obj[key].length;
-                    case 'boolean': return true;
-                    default: return obj[key][Object.keys(obj[key])[0]].length;
-                }
-            }) },
-            showingTokensFor(identity){
+            bind(changed, original) {
+                this[original] = changed;
+            },
+            fullKeysOf(obj) {
+                return Object.keys(obj).filter(key => {
+                    switch (typeof obj[key]) {
+                        case 'string':
+                            return obj[key].length;
+                        case 'boolean':
+                            return true;
+                        default:
+                            return obj[key][Object.keys(obj[key])[0]].length;
+                    }
+                });
+            },
+            showingTokensFor(identity) {
                 return this.showingTokens && (identity.publicKey === this.showingTokens.publicKey);
             },
-            async bindBalances(identity){
+            async bindBalances(identity) {
                 this.loadingTokenBalances = identity.publicKey;
                 let netAccountMap = [];
                 Object.keys(identity.accounts).map(netString =>
-                    netAccountMap.push({account:identity.accounts[netString], netString}));
+                    netAccountMap.push({account: identity.accounts[netString], netString}));
 
                 netAccountMap = ObjectHelpers.distinct(netAccountMap);
 
@@ -147,35 +158,37 @@
 
                 this.loadingTokenBalances = null;
             },
-            async accountBalances({account, netString}, identity){
+            async accountBalances({account, netString}, identity) {
                 let network = Network.fromUnique(netString);
 
-                if(!network.host){
+                if (!network.host) {
                     const hostedNetwork = this.scatter.settings.networks.find(n => n.chainId === network.chainId && n.host !== '');
-                    if(!hostedNetwork) return false;
+                    if (!hostedNetwork) return false;
                     network = hostedNetwork;
                 }
 
                 await PluginRepository.plugin(account.blockchain()).getBalances(account, network).then(balances => {
                     let idpkref = this.balances.find(bal => bal.idpk === identity.publicKey);
-                    if(!idpkref){
-                       this.balances.push({idpk:identity.publicKey, balances:[]});
-                       idpkref = this.balances.find(bal => bal.idpk === identity.publicKey);
+                    if (!idpkref) {
+                        this.balances.push({idpk: identity.publicKey, balances: []});
+                        idpkref = this.balances.find(bal => bal.idpk === identity.publicKey);
                     }
 
-                    idpkref.balances.push({network:netString, balances});
+                    idpkref.balances.push({network: netString, balances});
                     return true;
                 });
             },
-            balancesFor(identity){
+            balancesFor(identity) {
                 const identityBalances = this.balances.find(balances => balances.idpk === identity.publicKey);
-                if(!identityBalances) return [];
+                if (!identityBalances) return [];
                 return identityBalances.balances.map(b => b.balances);
             },
-            filterBySearch(){ return this.identities.filter(x => JSON.stringify(x).indexOf(this.searchText) > -1) },
-            removeIdentity(identity){
+            filterBySearch() {
+                return this.identities.filter(x => JSON.stringify(x).indexOf(this.searchText) > -1);
+            },
+            removeIdentity(identity) {
                 this[Actions.PUSH_ALERT](AlertMsg.RemovingIdentity(identity.name)).then(res => {
-                    if(!res || !res.hasOwnProperty('accepted')) return false;
+                    if (!res || !res.hasOwnProperty('accepted')) return false;
                     const scatter = this.scatter.clone();
                     scatter.keychain.identities = scatter.keychain.identities.filter(id => id.publicKey !== identity.publicKey);
                     scatter.keychain.permissions = scatter.keychain.permissions.filter(perm => perm.identity !== identity.publicKey);
@@ -183,43 +196,51 @@
                 });
 
             },
-            goToIdentity(identity){
-                this.$router.push({ name:RouteNames.IDENTITY, query: { publicKey: identity.publicKey } })
+            goToIdentity(identity) {
+                this.$router.push({name: RouteNames.IDENTITY, query: {publicKey: identity.publicKey}});
             },
-            createIdentity(){
-                this.$router.push({ name:RouteNames.IDENTITY, query: { publicKey: 'create' } })
+            createIdentity() {
+                this.$router.push({name: RouteNames.IDENTITY, query: {publicKey: 'create'}});
             },
-            networkToName(_network){
+            networkToName(_network) {
                 const network = this.scatter.settings.networks.find(network => network.unique() === _network);
-                if(!network) return 'Deleted Network';
+                if (!network) return 'Deleted Network';
                 return network.hasOwnProperty('name') && network.name.length ? network.name : network.unique();
+            },
+            explorer(_network) {
+                const network = this.scatter.settings.networks.find(network => network.unique() === _network);
+                return network.explorer();
             },
             ...mapActions([
                 Actions.UPDATE_STORED_SCATTER,
                 Actions.PUSH_ALERT,
             ])
         },
-        watch:{
-            showingTokens(){
+        watch: {
+            showingTokens() {
                 this.balances = [];
-                if(this.showingTokens)
-                    this.bindBalances(this.showingTokens)
+                if (this.showingTokens)
+                    this.bindBalances(this.showingTokens);
             }
         }
-    }
+    };
 </script>
 
 <style lang="scss">
 
     .token-balances {
         display: inline-block;
-        border-left: 10px solid rgba(0,0,0,0.1);
+        border-left: 10px solid rgba(0, 0, 0, 0.1);
         width: 100%;
         padding: 0 0 0 5px;
 
         .items {
-            margin-top:0 !important;
+            margin-top: 0 !important;
         }
+    }
+
+    .link-expo {
+        color: #54a7fc;
     }
 
 </style>
