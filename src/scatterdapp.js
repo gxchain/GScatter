77b96f9d343975e7ef9eeab063fc3f66f65138ad
刘宗源 <940101379@qq.com>
2019-03-13 12:02:40 +0800

    feat: add getArbitrarySignature

--- src/scatterdapp.js
+++ src/scatterdapp.js
@@ -193,14 +193,13 @@ export default class Scatterdapp {
      * @param whatfor
      * @param isHash - True if the data requires a hash signature
      */
-    // remove for temp
-    // getArbitrarySignature(publicKey, data, whatfor = '', isHash = false) {
-    //     return _send(NetworkMessageTypes.REQUEST_ARBITRARY_SIGNATURE, {
-    //         publicKey,
-    //         data,
-    //         whatfor,
-    //         isHash
-    //     }, true);
-    // }
+    getArbitrarySignature(publicKey, data, whatfor = '', isHash = false) {
+        return _send(NetworkMessageTypes.REQUEST_ARBITRARY_SIGNATURE, {
+            publicKey,
+            data,
+            whatfor,
+            isHash
+        }, true);
+    }
 
 }
--- src/services/SignatureService.js
+++ src/services/SignatureService.js
@@ -37,7 +37,7 @@ export default class SignatureService {
                     return false;
                 }
 
-                sendResponse(signature);
+                sendResponse(signature.toString('hex'));
             }, true, payload.isHash)
 
 
